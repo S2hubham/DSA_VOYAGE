99f75f4b7519b421954f23e3c59c68d149e8f2d9
Shubham Kokane <shubhamkokane151@gmail.com>
2024-10-05 19:33:43 +0530

    finding nth root of an integer

--- step-4 Binary Search/BS on 1D array/BS-11.cpp
+++ step-4 Binary Search/BS on 1D array/BS-11.cpp
@@ -3,33 +3,44 @@ using namespace std;
 
 
 // finding nth root of an integer
-int power(int mid, int root){
+
+// return 1 if == m
+// return 0 if < m
+// return 2 if > m
+int power(int mid, int n, int root){
+    long long ans = 1;
     for(int i = 0; i < root; i++){
-        
+        ans = ans * mid;
+        if(ans > n) return 2;
     }
+    if(ans == n) return 1;
+    return 0;
 }
 
 int sqrt_element(int n, int root){
+    if (n == 0 || n == 1) return n; 
+
     int low = 1, high = n;
     while(low <= high){
         int mid = low + (high - low)/2;
-        int val = power(mid, root);
+        int val = power(mid, n, root);
 
-        if(val == n){
+        if(val == 1){
             return mid;
         }
-        else if(val > n){
-            high = mid - 1;
+        else if(val == 0){
+            low = mid + 1;
         }
         else{
-            low = mid + 1;
+            high = mid - 1;
         }
     } 
     return -1;
+
+    // TC : O(log M) * O(root)
 }
 
 
 int main(){
-    cout<<sqrt_element(16, 4);
-    
+    cout<<sqrt_element(64, 2);
 }
