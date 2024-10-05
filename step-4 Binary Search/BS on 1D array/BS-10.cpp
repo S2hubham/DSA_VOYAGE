eb30869f5f176a30b8dc24905a0eedace72efed9
Shubham Kokane <shubhamkokane151@gmail.com>
2024-10-05 18:27:38 +0530

    finding (floor) sqrt of given number using binary search

---
+++ step-4 Binary Search/BS on 1D array/BS-10.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// finding sqrt of given number using binary search in an sorted array(range 1 to val)
+
+int sqrt_element(int val){
+    // brute force
+    // for(int i = 0; i < n; i++){
+    //     if(i*i == val){
+    //         return i;
+    //     }
+    // }
+    // TC : O(n)
+
+
+    // better
+    int low = 1, high = val;
+    long long ans = 1;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        
+        if(mid * mid <= val){
+            ans = mid;
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    // TC : O(log N)
+
+
+    return ans;
+}
+
+
+int main(){
+    cout<<sqrt_element(36);
+    
+}
