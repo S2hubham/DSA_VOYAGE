88e4d02346edca426f0892b30fbd00d11e959b49
Shubham Kokane <shubhamkokane151@gmail.com>
2024-10-16 11:46:32 +0530

    added best case for median

--- step-4 Binary Search/BS on answers/BS-12.cpp
+++ step-4 Binary Search/BS on answers/BS-12.cpp
@@ -38,7 +38,7 @@ int median(vector<int> arr1, vector<int> arr2)
 
 
     // better
-    int n1 = arr1.size(), n2 = arr2.size();
+    /* int n1 = arr1.size(), n2 = arr2.size();
     int n = n1 + n2;
     vector<int> ans;
     int i = 0, j = 0;
@@ -77,9 +77,40 @@ int median(vector<int> arr1, vector<int> arr2)
     else{
         return (idx1el + idx2el) / 2;
     }
-    return -1;
+    return -1; */
 
     // TC : O(n1 + n2)
+    // SC : O(1)
+
+
+    // best
+    int n1 = arr1.size(), n2 = arr2.size();
+    int n = n1 + n2;
+    if(n1 > n2) return median(arr2, arr1);
+    // symmetry formula
+    int x = (n1 + n2 + 1)/2;
+    int low = 0, high = n1;
+    while(low <= high){
+        int mid1 = low + (high - low) / 2;
+        int mid2 = x - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
+        if(mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
+        if(mid1 < n1) r1 = arr1[mid1];
+        if(mid2 < n2) r2 = arr2[mid2];
+        if(l1 <= r2 && l2 <= r1){
+            if(n % 2 != 0) return max(l1, l2);
+            else{
+                return (max(l1, l2) + min(r1, r2)) / 2;
+            }
+        } 
+        else if(l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return -1;
+
+    // TC : O(min(n1, n2))
     // SC : O(1)
 }
 
