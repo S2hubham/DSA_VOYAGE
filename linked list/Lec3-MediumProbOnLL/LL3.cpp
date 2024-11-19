53ca67d420d1c93469186620d3c70c4c174df967
Shubham Kokane <shubhamkokane151@gmail.com>
2024-11-19 18:31:38 +0530

    Detect loop in LL

---
+++ linked list/Lec3-MediumProbOnLL/LL3.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Reverse a LL
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterized Constructor
+    Node(int data)
+    {
+        // this ensure that actual value is passed and not the memory location
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *constructLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp; 
+        mover = temp;       
+    }
+
+    return head;
+}
+
+
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+
+// Iterative 
+bool detectloop(Node* head){
+    unordered_map<Node*, int> mpp;
+    Node* temp = head;
+    while(temp != NULL){
+        if(mpp.find(temp) == mpp.end()){
+            return true;
+        }
+        mpp[temp] = 1;
+        temp = temp->next;
+    }
+    return false;
+
+    // TC : O(n) only for traversal and not the insertion/fetching of data in unordered map
+    // SC : O(n)
+
+
+    // better (slow fast)
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    } 
+    return false;
+
+    // TC : O(n)  might be this as we dont know where fast will meet slow 
+    // SC : O(1)
+}
+
+
+int main()
+{
+    vector<int> arr1 = {1, 2, 3, 4, 5, 6};
+    Node* head = constructLL(arr1);
+
+    if (detectloop(head)) {
+        cout << "Loop detected in the linked list." << endl;
+    } else {
+        cout << "No loop detected in the linked list." << endl;
+    }
+    return 0;
+}
