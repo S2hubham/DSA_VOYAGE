0a2bc219c19a8cf873f7e3431603cb6ce4a8e1e3
Shubham Kokane <shubhamkokane151@gmail.com>
2025-02-14 20:33:14 +0530

    asteroid collisions

--- Stacks-Queues/2)monotonic/SQ6.cpp
+++ Stacks-Queues/2)monotonic/SQ6.cpp
@@ -11,13 +11,13 @@ vector<int> asteroid(vector<int> arr){
             st.push_back(arr[i]);
         }
         else{
-            while(!st.empty() && st.back() > 0 && st.back() < abs(arr[i])){
+            while(!st.empty() && st.back() > 0 && st.back() < abs(arr[i])){  // case 1
                 st.pop_back();
             }
-            if(!st.empty() && st.back() == abs(arr[i])){
+            if(!st.empty() && st.back() == abs(arr[i])){  // case 2
                 st.pop_back();
             }
-            else if(st.empty() || st.back() < 0){
+            else if(st.empty() || st.back() < 0){    // case 3
                 st.push_back(arr[i]);
             }
         }
@@ -27,8 +27,8 @@ vector<int> asteroid(vector<int> arr){
 
 int main(){
     vector<int> arr1 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -19, -20, 70};
-    vector<int> arr2 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -19};
-    vector<int> arr3 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -19, -30};
+    vector<int> arr2 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -19};   // case 1 (for -19), case 2(for 7 <-> -7)
+    vector<int> arr3 = {4, 7, 1, 1, 2, -3, -7, 17, 15, -19, -30};  //case 3 (after -19 push -ve values)
     vector<int> arr4 = {4, 7, 1, 1, 2, -3, -7, 17, 15};
 
     vector<int> ans = asteroid(arr1);
