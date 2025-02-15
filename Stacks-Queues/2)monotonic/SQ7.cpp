5a3c6a274de358d47121f317fce7a29a2dac227a
Shubham Kokane <shubhamkokane151@gmail.com>
2025-02-15 19:26:13 +0530

    sum of subarray minimums

---
+++ Stacks-Queues/2)monotonic/SQ7.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// sum of subarray minimums
+vector<int> findnse(vector<int> arr){
+    stack<int> st;
+    vector<int> NSE(arr.size(), arr.size());
+    for(int i = arr.size()-1; i >= 0; i--){
+        while(!st.empty() && arr[st.top()] >= arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            NSE[i] = st.top();
+        }
+        st.push(i);
+    }
+    return NSE;
+    // TC : O(2n)
+    // SC : O(n)
+}
+
+
+vector<int> findpse(vector<int> arr){
+    stack<int> st;
+    vector<int> PSE(arr.size(), -1);
+    for(int i = 0; i < arr.size(); i++){
+        while(!st.empty() && arr[st.top()] > arr[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+            PSE[i] = st.top();
+        }
+        st.push(i);
+    }
+    return PSE;
+    // TC : O(2n)
+    // SC : O(n)
+}
+
+
+void sum_subarray_min(vector<int> arr){
+    // brute force
+    /* int total = 0;
+    for(int i = 0; i < arr.size(); i++){
+        int minele = INT_MAX;
+        for(int j = i; j < arr.size(); j++){
+            minele = min(minele, arr[j]);
+            total += minele;
+        }
+    } 
+    cout<<"Total : "<<total; */
+
+    // TC : O(n^2)
+    // SC : O(1)
+
+
+
+    // better
+    vector<int> nse = findnse(arr);
+    vector<int> pse = findpse(arr);
+    int total = 0;
+    for(int i = 0; i < arr.size(); i++){
+        int left = arr[i] - pse[i];
+        int right = nse[i] - arr[i];
+        total += (left * right * arr[i]);
+    }
+    cout<<"Total : "<<total;
+
+    // TC : O(5n)
+    // SC : O(2n)
+}
+
+int main(){
+    vector<int> arr = {1, 1};
+    sum_subarray_min(arr);
+    return 0;
+} 
