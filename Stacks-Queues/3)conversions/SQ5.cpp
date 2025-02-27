08d89333cd7a3317e42caa5a438dcd3e135d00d0
Shubham Kokane <shubhamkokane151@gmail.com>
2025-02-27 09:24:17 +0530

    Postfix -> Prefix

---
+++ Stacks-Queues/3)conversions/SQ5.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Postfix to Prefix
+string reverse(string s){
+    string rev;
+    for(int i = s.size()-1; i >= 0; i--){
+        char c = s[i];
+        if(isalnum(c)){
+            rev += c;
+        }
+        else if(c == '('){
+            rev += ')';
+        }
+        else if(c == ')'){
+            rev += '(';
+        }
+        else{
+            rev += c;
+        }
+    }
+    return rev;
+}
+
+void Postfix_Prefix(string exp)
+{
+    int i = 0;
+    stack<string> st;
+    while(i < exp.size()){
+        char c = exp[i];
+        if(isalnum(c)){
+            st.push(string(1, c));
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+            string con = c + t2 + t1;
+            st.push(con);
+        }
+        i++;
+    }
+    cout<<"Prefix -> "<<st.top();
+    st.pop();
+}
+
+int main() {
+    vector<string> test_cases = {
+        "abc*+",
+        "ab+c*",
+        "abcd^e-*+",
+        "abc*d/+",
+        "abc*d/ef^-g*-h+",
+        "ab+cd+*",
+        "abc^^",
+        "abc*+d+",
+        "abc-d+e/*",
+        "abc*+d+",
+        "abc*d/ef^-g*-h+",
+        "hmq74-^^^"
+    };
+    
+
+    for (const string &exp : test_cases) {
+        cout<<"Postfix -> "<<exp<<endl;
+        Postfix_Prefix(exp);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
