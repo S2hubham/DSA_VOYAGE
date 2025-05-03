c3ced327ca7541d7419c6cc1614eaea3d3258cd0
Shubham Kokane <shubhamkokane151@gmail.com>
2025-05-03 19:56:44 +0530

    Iterative In-Order traversal

---
+++ steps-7-Binary-trees/1.traversals/7.iterativeInorderTraversal.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Iterative In-Order traversal on Binary tree
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+void itr_inorder_traversal(Node* root){
+    stack<Node*> st;
+    Node* node = root;
+
+    while(node || !st.empty()){
+        if(node){
+            st.push(node);
+            node = node->left;
+        }
+        else{
+            // if(st.empty())  break;
+            node = st.top();
+            st.pop();
+            cout<<node->data<<" ";
+            node = node->right;
+        }
+    }
+    return;
+
+    // TC : O(n)
+    // SC : O(n)
+}
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5); 
+    root->left->right->left = new Node(8); 
+
+    root->right = new Node(3);
+    root->right->left = new Node(6); 
+    root->right->right = new Node(7);
+    root->right->right->left = new Node(9);  
+    root->right->right->right = new Node(10); 
+
+    itr_inorder_traversal(root);
+    
+    return 0;
+}
