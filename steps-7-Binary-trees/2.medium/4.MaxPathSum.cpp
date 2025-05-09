c041447a72a27f7863a4c20d6a44cf48e265d45e
Shubham Kokane <shubhamkokane151@gmail.com>
2025-05-09 23:20:49 +0530

    Maximum path sum of Binary Tree

---
+++ steps-7-Binary-trees/2.medium/4.MaxPathSum.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Maximum path sum of Binary Tree
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+int func(Node* root, int &maxi){
+    // base case 
+    if(root == NULL)    return 0;
+
+    int leftsum = func(root->left, maxi);
+    int rightsum = func(root->right, maxi);
+
+    maxi = max(maxi, leftsum + root->data + rightsum);  // this is to keep the max path
+
+    if(leftsum < 0) leftsum = 0;
+    if(rightsum < 0)    rightsum = 0;
+    return (root->data + max(leftsum, rightsum));  // return only best path with above optimisation
+
+    // TC : O(n)
+    // SC : O(h) ~= O(n)
+}
+
+
+int main(){
+    Node* root = new Node(15);
+    root->left = new Node(10); 
+
+    root->right = new Node(20);
+    root->right->left = new Node(-30); 
+    root->right->right = new Node(-15);
+
+    int maxi = 0;
+    func(root, maxi);
+    cout<<"Maximum path : "<<maxi;
+
+    return 0;
+}
