41aeaef9559d7c957dd67dde5b7fbf51783ca1b6
Shubham Kokane <shubhamkokane151@gmail.com>
2025-05-11 19:15:02 +0530

    Bottom view of Binary Tree

---
+++ steps-7-Binary-trees/2.medium/10.BottomView.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Bottom view of Binary Tree
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+vector<int> bottomView(Node* root){
+    if(!root)   return {};
+    queue<pair<Node*, int>> q;    // q(node, vertical line)
+    map<int, int> mpp;            // mpp(vertical line, node)
+
+    q.push({root, 0});
+
+    while(!q.empty()){
+        auto p = q.front();
+        q.pop();
+        
+        Node* temp = p.first;
+        int line = p.second;
+
+        mpp[line] = temp->data;
+
+        if(temp->left)  q.push({temp->left, line-1});
+        if(temp->right) q.push({temp->right, line+1});
+    }
+
+    vector<int> ans;
+    for(auto it : mpp){
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5); 
+    root->left->right->left = new Node(8); 
+    root->left->right->right = new Node(9);  
+ 
+
+    root->right = new Node(3);
+    root->right->left = new Node(6); 
+    root->right->right = new Node(7); 
+
+    vector<int> ans = bottomView(root);
+
+    for(int i = 0; i < ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    
+    return 0;
+}
