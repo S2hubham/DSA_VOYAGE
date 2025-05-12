58c3ffcb9e1d3d317497075cb24d7e6ee17f65dd
Shubham Kokane <shubhamkokane151@gmail.com>
2025-05-12 18:52:09 +0530

    Left view of Binary Tree

---
+++ steps-7-Binary-trees/2.medium/12.LeftView.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Left view of Binary Tree
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// Iterative approach
+/* vector<int> leftView(Node* root){
+    vector<int> ans;
+    if(!root)   return ans;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        int size = q.size();
+
+        for(int i = 0; i < size; i++){
+            Node* temp = q.front();
+            q.pop();
+
+            if(i == 0)  ans.push_back(temp->data);
+
+            if(temp->left)  q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+
+    return ans;
+
+    // TC : O(n)
+    // SC : O(n)
+}  */
+
+
+
+// recursive approach
+void func(Node* root, int lvl, vector<int> &ans){
+    // base case
+    if(!root)   return;
+
+    // main step
+    if(ans.size() == lvl){
+        ans.push_back(root->data);
+    }
+
+    func(root->left, lvl + 1, ans);
+    func(root->right, lvl + 1, ans);
+}
+
+vector<int> leftView(Node* root){
+    vector<int> ans;
+    func(root, 0, ans);
+    return ans;
+
+    // TC : O(n)
+    // SC : O(n)
+} 
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5); 
+    root->left->right->left = new Node(8); 
+    root->left->right->right = new Node(9);  
+ 
+
+    root->right = new Node(3);
+    root->right->left = new Node(6); 
+    root->right->right = new Node(7); 
+
+    vector<int> ans = leftView(root);   // 1 2 4 8
+
+    for(int i = 0; i < ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    
+    return 0;
+}
