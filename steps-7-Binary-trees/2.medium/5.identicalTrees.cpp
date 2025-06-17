45e39f66a1b97d1acd7e79f4d290419d522b2aad
Shubham Kokane <shubhamkokane151@gmail.com>
2025-06-17 18:58:26 +0530

    correction in Identical Binary Tree

--- steps-7-Binary-trees/2.medium/5.identicalTrees.cpp
+++ steps-7-Binary-trees/2.medium/5.identicalTrees.cpp
@@ -20,7 +20,7 @@ bool identical(Node* root1, Node* root2){
         return (root1 == root2);
     }
 
-    return ((root1->data != root2->data) 
+    return ((root1->data == root2->data) 
     && identical(root1->left, root2->left) 
     && identical(root1->right, root2->right));
 
@@ -60,7 +60,7 @@ int main(){
     root1->left = new Node(2);
     root1->left->left = new Node(4); 
     root1->left->right = new Node(5); 
-    // root->left->right->left = new Node(8); missing this one
+    root1->left->right->left = new Node(8); //missing this one
 
     root1->right = new Node(3);
     root1->right->left = new Node(6); 
